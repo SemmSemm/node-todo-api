60c3df7613b94169e0598b7a1c49466c7d6c5a3e
SemmSemm <sema.lukinyh@gmail.com>
2018-11-01 18:21:46 +0100

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,52 @@
+const request = require('supertest');
+const expect = require('expect');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+// before each test run we clear data from todos collection in database
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+            .post('/todos')
+            .send({text})   // sending our object
+            .expect(200)    // expects status code 200 (OK)
+            .expect((res) => { // expecting our response body with property 'text'  
+                expect(res.body.text).toBe(text); // be same as text property in toBe()
+            })
+            .end((err, res) => {
+                if(err) {   // if there is some errors, we ending scrypt with error
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should not create todo with invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+                
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
