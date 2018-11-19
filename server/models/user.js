33b286b7bb0edbc2a4de82de1173726c3222a46f
SemmSemm <sema.lukinyh@gmail.com>
2018-11-19 19:36:01 +0100

    auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,7 +32,7 @@ var UserSchema = new mongoose.Schema({
             required: true
         },
         token: {
-            type: Number,
+            type: String,
             required: true
         }
     }]
@@ -68,7 +68,22 @@ UserSchema.methods.generateAuthToken = function () {
 };
 
 // TODO: findByToken
-// UserSchema.statics.findByToken();
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    })
+};
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -142,10 +143,8 @@ app.post('/users', (req, res) => {
 /**
  * Get information about user
  */
-app.get('/users/me', (req, res) => {
-    var token = req.header('x-auth');
-
-    User.findByUser(token);
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
